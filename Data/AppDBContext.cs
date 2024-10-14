db71df2b14cf8aad94e69fe649a9259106f44919
hungsilver <hungsilvertq@gmail.com>
2024-10-14 14:34:15 +0700

    first commit

---
+++ Data/AppDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QLDD.Models;
+
+namespace QLDD.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("User");
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QLDD.Models
+{
+    public class User
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [StringLength(50)]
+        public string? FirstMidName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public int? BirthYear { get; set; }
+        public int Gender { get; set; }
+        [StringLength(20)]
+        public string? TaxCode { get; set; }
+        [StringLength(5)]
+        public string? NationalityCode { get; set; }
+        [StringLength(30)]
+        public string? NationalityNameTV { get; set; }
+        [StringLength(30)]
+        public string? NationalityNameQT { get; set; }
+        [StringLength(30)]
+        public string? Nation { get; set; }
+        public int TypeOfIdentificationDocs { get; set; }
+        public DateTime? IssueDate { get; set; }
+        [StringLength(100)]
+        public string? IssuePlace { get; set; }
+        public int? AddressID { get; set; }
+        public int? IdentificationID { get; set; }
+        public int Status { get; set; }
+    }
+}
